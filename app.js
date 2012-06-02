3764cbaeed09953d66c7bc2a264d8a0ec08296e7
babs <sundeep@gsundeep.com>
2012-06-02 06:49:22 -0400

    Added tons of shit

--- app.js
+++ app.js
@@ -4,29 +4,32 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
-
+    
 var app = module.exports = express.createServer();
+var io = require('socket.io').listen(app);
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
 // Routes
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(1500);
+
+// TODO: FILTER INPUT
+
+io.sockets.on('connection', function (socket) {
+  socket.on('message', function (data) {
+  	data = data.replace(/<(?:.|\n)*?>/gm, '');
+    var user = socket.handshake.address;
+	socket.broadcast.emit('new', { "message": data, "ip": user.address });
+  });
+  socket.on('image', function (data) {
+  	data = data.replace(/<(?:.|\n)*?>/gm, '');
+    var user = socket.handshake.address;
+	socket.broadcast.emit('new_image', { "image": data, "ip": user.address });
+  });
 });
--- public/main.js
+++ public/main.js
@@ -1,8 +1,5 @@
-DEBUG_MODE = true;
-
 $(document).ready(function() {
-	var upload_box = document.getElementById("upload_box")
-
+	var upload_box = document.getElementById("chat");
 	upload_box.addEventListener("dragenter", dragEnter, false);
 	upload_box.addEventListener("dragexit", dragExit, false);
 	upload_box.addEventListener("dragover", dragOver, false);
@@ -12,10 +9,6 @@ $(document).ready(function() {
 function stopEvents(evt) {
 	evt.stopPropagation();
 	evt.preventDefault();
-}
-
-function log(msg) {
-	if(DEBUG_MODE) console.log(msg);
 }
 
 function dragEnter(evt) {
@@ -31,7 +24,6 @@ function dragOver(evt) {
 }
 
 function drop(evt) {
-	log("drop");
 	stopEvents(evt);
 
 	var files = evt.dataTransfer.files;
@@ -42,26 +34,16 @@ function drop(evt) {
 }
 
 function handleFiles(files) {
-	log("handleFiles");
 	var first_file = files[0];
 	window.first_file_name = first_file.name;
 	window.reader = new FileReader();
 
-	window.reader.onprogress = handleReaderProgress;
 	window.reader.onloadend = handleReaderLoadEnd;
 	window.reader.readAsDataURL(first_file);
 }
 
-function handleReaderProgress(evt) {
-	log("handleProgress");
-	if (evt.lengthComputable) {
-		var loaded = (evt.loaded / evt.total);
-		document.getElementById("upload_label").innerHTML = "Sending: " + window.first_file_name + ", " + loaded + "% complete";
-	}
-}
-
 function handleReaderLoadEnd(evt) {
-	log("handleLoadEnd");
-	var img = document.getElementById("upload_display");
-	img.src = evt.target.result;
+	// Append image to page.
+	$('#transcript').append("<div class='rec_message'><span class='me'>Me</span>: <img OnLoad='scrollToBottom();' src='" + evt.target.result + "' /></div><br>");
+	socket.emit('image', evt.target.result);
 }
