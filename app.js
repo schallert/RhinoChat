e02019b1e6bb78310914b36bb7d69d91fd3eb5b6
babs <sundeep@gsundeep.com>
2012-06-02 14:35:35 -0400

    Tried adding another limit. Failed again. There needs to be a way to find the size of the whole buffer.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.configure(function(){
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
   // Doesn't work
-  app.use(express.limit('2mb'));
+  // app.use(express.limit('2mb'));
 });
 
 // Routes
@@ -24,14 +24,23 @@ app.listen(1500);
 // TODO: FILTER INPUT
 
 io.sockets.on('connection', function (socket) {
+
   socket.on('message', function (data) {
+    
+    // if (data.length > utils.parseBytes("2mb")) socket.destroy();
+    
   	data = data.replace(/<(?:.|\n)*?>/gm, '');
     var user = socket.handshake.address;
 	socket.broadcast.emit('new', { "message": data, "ip": user.address });
   });
+  
   socket.on('image', function (data) {
+  
+    // if (data.length > utils.parseBytes("2mb")) socket.destroy();
+    
   	data = data.replace(/<(?:.|\n)*?>/gm, '');
     var user = socket.handshake.address;
 	socket.broadcast.emit('new_image', { "image": data, "ip": user.address });
   });
+  
 });
