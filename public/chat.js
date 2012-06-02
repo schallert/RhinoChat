3883c188fb59cf5fb4cf1bd8b0c8bd669ebc7189
Neal Bhasin <thisisneal@gmail.com>
2012-06-02 14:35:51 -0700

    config support
    config file for production and local testing, fixed readme for merge
    conflict

---
+++ public/chat.js
@@ -0,0 +1,27 @@
+// not currently in use yet 
+
+var socket = io.connect('localhost:1500/');
+
+socket.on('new', function (data) {
+    $('#transcript').append("<div class='rec_message'><span class='other'>" + data.ip + "</span>: " + data.message + "</div>");
+    scrollToBottom();
+});
+
+socket.on('new_image', function (data) {
+    $('#transcript').append("<div class='rec_message'><span class='other'>" + data.ip + "</span>: <img OnLoad='scrollToBottom();' src='" + data.image + "' /></div>");
+});
+
+function send_message() {
+	if($('#message').val()!="") {
+  		var message = $('#message').val().replace(/<(?:.|\n)*?>/gm, '');
+  		$('#transcript').append("<div class='rec_message'><span class='me'>Me</span>: " + message + "</div>");
+  		$("#transcript").scrollTop($("#transcript")[0].scrollHeight);
+  		$('#message').val("");
+  		scrollToBottom();
+  		socket.emit('message', message);
+	}
+}
+
+function scrollToBottom() {
+	$("#transcript").scrollTop($("#transcript")[0].scrollHeight);
+}
