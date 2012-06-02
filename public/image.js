9e6d08cf968460d2854b5643c8fe517399145cbb
babs <sundeep@gsundeep.com>
2012-06-02 07:04:22 -0400

    made chat loook a bit nicer. made it so you have to enter something before anything is added.

---
+++ public/image.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+	var upload_box = document.getElementById("chat");
+	upload_box.addEventListener("dragenter", dragEnter, false);
+	upload_box.addEventListener("dragexit", dragExit, false);
+	upload_box.addEventListener("dragover", dragOver, false);
+	upload_box.addEventListener("drop", drop, false);
+});
+
+function stopEvents(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+}
+
+function dragEnter(evt) {
+	stopEvents(evt);
+}
+
+function dragExit(evt) {
+	stopEvents(evt);
+}
+
+function dragOver(evt) {
+	stopEvents(evt);
+}
+
+function drop(evt) {
+	stopEvents(evt);
+
+	var files = evt.dataTransfer.files;
+	var num_files = files.length;
+
+	if (num_files > 0)
+		handleFiles(files);
+}
+
+function handleFiles(files) {
+	var first_file = files[0];
+	window.first_file_name = first_file.name;
+	window.reader = new FileReader();
+
+	window.reader.onloadend = handleReaderLoadEnd;
+	window.reader.readAsDataURL(first_file);
+}
+
+function handleReaderLoadEnd(evt) {
+	// Append image to page.
+	$('#transcript').append("<div class='rec_message'><span class='me'>Me</span>: <img OnLoad='scrollToBottom();' src='" + evt.target.result + "' /></div>");
+	socket.emit('image', evt.target.result);
+}
